38fdd8c7cf1e9589a236532d6f901a0cb53bfbe5
ovorlova <olia_orlova2000@mail.ru>
2020-05-10 22:36:30 +0300

    opts.py and logger.py

--- src/lib/opts.py
+++ src/lib/opts.py
@@ -82,7 +82,7 @@ class opts(object):
     # train
     self.parser.add_argument('--lr', type=float, default=1.25e-4, 
                              help='learning rate for batch size 32.')
-    self.parser.add_argument('--lr_step', type=str, default='90,120',
+    self.parser.add_argument('--lr_step', type=str, default='1000',
                              help='drop learning rate by 10.')
     self.parser.add_argument('--num_epochs', type=int, default=140,
                              help='total training epochs.')
@@ -105,7 +105,7 @@ class opts(object):
                              help='multi scale test augmentation.')
     self.parser.add_argument('--nms', action='store_true',
                              help='run nms in testing.')
-    self.parser.add_argument('--K', type=int, default=25,
+    self.parser.add_argument('--K', type=int, default=11,
                              help='max number of output objects.') 
     self.parser.add_argument('--not_prefetch_test', action='store_true',
                              help='not use parallal data pre-processing.')
@@ -115,7 +115,8 @@ class opts(object):
     self.parser.add_argument('--keep_res', action='store_true',
                              help='keep the original resolution'
                                   ' during validation.')
-
+    self.parser.add_argument('--score', type=float, default=0.0,
+                             help='delete scores below mentioned.')
     # dataset
     self.parser.add_argument('--not_rand_crop', action='store_true',
                              help='not use the random crop data augmentation'
